6142a3978223b79337e481a68fb98b817ca559e6
market-predictions <darko.desktop@gmail.com>
2026-07-21 17:06:15 +0200

    Export market-gap audit helpers

--- src/dtr_lab/data/__init__.py
+++ src/dtr_lab/data/__init__.py
@@ -1,4 +1,13 @@
+from dtr_lab.data.gaps import GapAuditSummary, classify_gaps, summarize_gaps
 from dtr_lab.data.loader import load_market_data, resample_ohlcv
 from dtr_lab.data.validation import DataAudit, audit_market_data
 
-__all__ = ["DataAudit", "audit_market_data", "load_market_data", "resample_ohlcv"]
+__all__ = [
+    "DataAudit",
+    "GapAuditSummary",
+    "audit_market_data",
+    "classify_gaps",
+    "load_market_data",
+    "resample_ohlcv",
+    "summarize_gaps",
+]
