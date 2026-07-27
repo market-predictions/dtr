d657ebb089f01cf04425cd46862148cc30c9e827
market-predictions <darko.desktop@gmail.com>
2026-07-27 02:30:27 +0200

    Expose H4 attribution API

--- src/dtr_lab/strategies/wayne_direction/__init__.py
+++ src/dtr_lab/strategies/wayne_direction/__init__.py
@@ -8,6 +8,11 @@ from .calendar import (
     pivot_day_start,
 )
 from .data import PRIMARY_PAIRS, load_bid_ask_minutes, midpoint_minutes
+from .h4_sensitivity import (
+    build_h4_monthly_attribution,
+    classify_d1_relation,
+    classify_h4_relation,
+)
 from .health import attach_last_completed_h4, build_h4_health
 from .model import CandidateState, TrendConfig, TrendDirection
 from .monthly import (
@@ -27,11 +32,14 @@ __all__ = [
     "attach_last_completed_h4",
     "average_true_range",
     "build_h4_health",
+    "build_h4_monthly_attribution",
     "build_monthly_context",
     "build_monthly_reach_ledger",
     "build_new_york_daily_bars",
     "build_new_york_h4_bars",
     "build_structure_ledger",
+    "classify_d1_relation",
+    "classify_h4_relation",
     "confirmed_swings",
     "h4_bar_start",
     "load_bid_ask_minutes",
