e4b9cdc1be057b62ce7c49d61c1fb032061a2f2c
market-predictions <darko.desktop@gmail.com>
2026-08-05 16:10:51 +0200

    Add Quarters Theory package

---
+++ src/dtr_lab/strategies/quarters_theory/__init__.py
@@ -0,0 +1,21 @@
+"""Quarters Theory falsification and distinctiveness research tools."""
+
+from .stage1 import (
+    HORIZONS_MINUTES,
+    PRIMARY_RESET_PIPS,
+    audit_year,
+    bootstrap_phase_effect,
+    detect_events,
+    load_year,
+    stratified_phase_effect,
+)
+
+__all__ = [
+    "HORIZONS_MINUTES",
+    "PRIMARY_RESET_PIPS",
+    "audit_year",
+    "bootstrap_phase_effect",
+    "detect_events",
+    "load_year",
+    "stratified_phase_effect",
+]
